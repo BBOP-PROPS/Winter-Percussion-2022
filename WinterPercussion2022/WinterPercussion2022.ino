b1ee965645cd2ff39c4e6a853583dcf1e3a4261c
scottmdial <90105099+scottmdial@users.noreply.github.com>
2022-04-04 22:55:19 -0400

    modified to constant speed.

--- WinterPercussion2022/WinterPercussion2022.ino
+++ WinterPercussion2022/WinterPercussion2022.ino
@@ -8,7 +8,7 @@
 
 */
 
-#define __DEBUG__
+//#define __DEBUG__
 #ifdef __DEBUG__
    #define print(...)   Serial.print(__VA_ARGS__)
    #define println(...) Serial.println(__VA_ARGS__)
@@ -32,17 +32,12 @@
 
 #define dirPin 5        // DIR+
 #define stepPin 7       // PUL+
-#define recD0 9        // From wireless receiver1 -- Xmtr Pin = 4
-#define recD1 10        // From wireless receiver2 -- Xmtr Pin = 3
-#define recD2 11        // From wireless receiver3 -- Xmtr Pin = 1
-#define recD3 12        // From wireless receiver4 -- Xmtr Pin = 2
 #define downOverride 6
 #define upOverride 8
 #define returnPin 11  
 #define motorInterfaceType 1
 
-int homePos = 0;
-int tmp = 0;
+
 
 // Create a new instance of the AccelStepper class:
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
@@ -53,8 +48,11 @@ void setup() {
   pinMode(downOverride, INPUT_PULLUP); 
   pinMode(upOverride, INPUT_PULLUP);
   #ifdef __DEBUG__
-     Serial.begin(9600);
+     Serial.begin(115200);
   #endif
+  println("Setting Max Speed to 7500");
+  stepper.setMaxSpeed(7500);
+  stepper.setSpeed(1000);
 
 }
 
@@ -74,29 +72,20 @@ void loop() {
               if (digitalRead(downOverride) == 0 )     
               {      
                while (digitalRead(downOverride) == 0) {
-                  homePos=tmp;
                   println(" Entering Manual Rolldown Mode");
-                  digitalWrite(LED_BUILTIN, LOW); 
-                  tmp = tmp - 1500;
-                  print("Value: ");
-                  println(tmp);
-                  stepper.setMaxSpeed(7500);
-                  stepper.setAcceleration(100000);
-                  stepper.moveTo(tmp);
-                  stepper.runToPosition();
-                  
+                  digitalWrite(LED_BUILTIN, LOW);     
+                 stepper.setSpeed(1000);
+                 stepper.runSpeed();  
                }
-               homePos=tmp;
               } else { 
                     println ("Waiting 52 more Seconds");
                     delay (52000);
                     println ("Roll Down Fabric");
-                    stepper.setMaxSpeed(7500); // 600 = 12s ;  800 = 10 sec; 1000= 8 sec
                     stepper.setAcceleration(10000);
                     stepper.moveTo(-42000); 
                     stepper.runToPosition();
                     delay(38000);
-                    stepper.moveTo(homePos);
+                    stepper.moveTo(0);
                     stepper.runToPosition();
               }
       } 
@@ -105,17 +94,10 @@ void loop() {
   // if (digitalRead(upOverride) == 0 )  || digitalRead(recD3) == HIGH) {  
  
   if (digitalRead(upOverride) == 0 ) { 
-        tmp=homePos;
-         homePos = homePos + 1500;
         println ("Roll-up Fabric");
-        print("Value: ");
-        println(homePos);
-        stepper.setMaxSpeed(7500);
-        stepper.setAcceleration(100000);
-        stepper.moveTo(homePos);
-        stepper.runToPosition();
+        stepper.setSpeed(-1000);
+        stepper.runSpeed();  
         stepper.setCurrentPosition(0);
-
       } 
       
   //stepper.runToPosition();
